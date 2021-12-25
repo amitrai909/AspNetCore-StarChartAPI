7bbd8615360ca0ce9841a9b988c9d1c7e78cdc65
amitra909 <amit-rai@outlook.com>
2021-12-24 17:19:58 -0800

    updates class name

--- StarChart/Models/CelestialObject.cs
+++ StarChart/Models/CelestialObject.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace StarChart.Models
 {
-    public class CelestialObject 
+    public class CelestialObject
     {
         public int Id { get; set; }
         public string Name  { get; set; }
